3c9863f7e603a76dea74dafacc3bc61ddd2fe2a6
akosiRIINIERU <maghanoy.renhiel765@gmail.com>
2025-10-01 09:40:30 +0800

    croochie

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 
   import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
-  import "./index.css";
+  import App from "C:/New folder (5)/telehealth/src/App.tsx";
+  import "C:/New folder (5)/telehealth/src/index.css";
 
   createRoot(document.getElementById("root")!).render(<App />);
   
