d35230991d947ed32832c3734d26e5b9fb59fc7e
akosiRIINIERU <maghanoy.renhiel765@gmail.com>
2025-10-01 09:55:37 +0800

    asd

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Tele-Health/"
+  base: "/Tele-Health/",
+  build: {chunkSizeWarningLimit: 2000, 
+  }
 })
